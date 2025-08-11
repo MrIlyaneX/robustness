fix: fix accuracy calculation and logging

robustness/tools/helpers.py
@@ -48,50 +48,23 @@ def ckpt_at_epoch(num):
 
 def accuracy(output, target, topk=(1,), exact=False):
     """
-    Computes the top-k accuracy for the specified values of k
+    Computes the accuracy for multi-class classification.
 
     Args:
-        output (ch.tensor) : model output (N, classes) or (N, attributes)
-            for sigmoid/multitask binary classification
-        target (ch.tensor) : correct labels (N,) [multiclass] or (N,
-            attributes) [multitask binary]
-        topk (tuple) : for each item "k" in this tuple, this method
-            will return the top-k accuracy
-        exact (bool) : whether to return aggregate statistics (if
-            False) or per-example correctness (if True)
+        output (ch.Tensor): The model's logits or scores for each class.
+                            Shape: (N, C), where N is the batch size and C is the number of classes.
+        target (ch.Tensor): The ground truth labels (class indices).
+                            Shape: (N,), where each value is an integer from 0 to C-1.
 
     Returns:
-        A list of top-k accuracies.
+        float: The classification accuracy as a percentage.
     """
     with ch.no_grad():
-        # Binary Classification
-        if len(target.shape) > 1:
-            assert output.shape == target.shape, (
-                "Detected binary classification but output shape != target shape"
-            )
-            return [ch.round(ch.sigmoid(output)).eq(ch.round(target)).float().mean()], [
-                -1.0
-            ]
+        pred = ch.argmax(output, dim=1)
+        correct = pred.eq(target)
+        accuracy = correct.float().mean().item() * 100.0
 
-        maxk = max(topk)
-        batch_size = target.size(0)
-
-        _, pred = output.topk(maxk, 1, True, True)
-        pred = pred.t()
-        correct = pred.eq(target.view(1, -1).expand_as(pred))
-
-        res = []
-        res_exact = []
-        for k in topk:
-            correct_k = correct[:k].reshape(-1).float()
-            ck_sum = correct_k.sum(0, keepdim=True)
-            res.append(ck_sum.mul_(100.0 / batch_size))
-            res_exact.append(correct_k)
-
-        if not exact:
-            return res
-        else:
-            return res_exact
+    return accuracy
 
 
 class InputNormalize(ch.nn.Module):